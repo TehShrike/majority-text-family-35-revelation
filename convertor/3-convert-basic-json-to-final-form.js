ffc107e65bcf21b16c2e262191117a4a346f7b85
Josh Duff <me@JoshDuff.com>
2017-03-10 12:16:59 -0600

    A parsed version that might work?

---
+++ convertor/3-convert-basic-json-to-final-form.js
@@ -0,0 +1,175 @@
+const fs = require('fs')
+
+const input = require('../very-basic-parsed.json')
+
+write('revelation', process(input))
+
+function process(basic) {
+	// no trimming
+	// if there's a note reference between verse chunks, add a space
+	// if there's a line break between verse chunks, add a space
+
+	// "between verses" = no chapter numbers or paragraph breaks before the next verse chunk
+
+	let currentChapter = 1
+	let currentVerse = 1
+	let currentSection = 1
+
+	const output = []
+
+	concatenateVerseChunks(basic).forEach(chunk => {
+		if (chunk.type === 'chapter number') {
+			currentChapter = parseInt(chunk.text.trim(), 10)
+			currentVerse = 1
+			currentSection = 1
+		} else if (chunk.type === 'verse number') {
+			currentVerse = parseInt(chunk.text.trim(), 10)
+			currentSection = 1
+		} else if (chunk.type === 'verse') {
+			output.push({
+				chapterNumber: currentChapter,
+				verseNumber: currentVerse,
+				sectionNumber: currentSection,
+				text: chunk.text
+			})
+			currentSection++
+		} else if (chunk.type === 'paragraph break') {
+			output.push(chunk)
+		} else {
+			throw new Error(`not handling chunk type ${chunk.type}`)
+		}
+	})
+
+	return output
+}
+
+function concatenateVerseChunks(basic) {
+	const output = []
+	let currentVerseChunkText = ''
+
+	function finishCurrentChunk() {
+		if (currentVerseChunkText) {
+			output.push({
+				type: 'verse',
+				text: currentVerseChunkText
+			})
+			currentVerseChunkText = ''
+		}
+	}
+
+	specialForEach(basic, (chunk, tail) => {
+		if (chunk.type === 'verse') {
+			currentVerseChunkText += chunk.text
+			if (nextChunkThatMattersNeedsAtLeastOneSpace(tail)) {
+				currentVerseChunkText += ' '
+			}
+		} else if (chunk.type === 'chapter number' ||
+				chunk.type === 'verse number' ||
+				chunk.type === 'paragraph break') {
+			finishCurrentChunk()
+			output.push(chunk)
+		}
+	})
+	finishCurrentChunk()
+
+	return output
+}
+
+
+function specialForEach(array, fn) {
+	array.forEach((element, index) => {
+		fn(element, array.slice(index + 1))
+	})
+}
+
+function needAtLeastOneSpace(nextChunk) {
+	return nextChunk.type === 'chapter number'
+		|| nextChunk.type === 'paragraph break'
+		|| nextChunk.type === 'note reference'
+		|| nextChunk.type === 'line break'
+}
+
+function nextChunkThatMattersNeedsAtLeastOneSpace(chunks) {
+	if (chunks.length === 0) {
+		return false
+	}
+
+	const [ chunk, ...tail ] = chunks
+
+	const doesntCount = chunk.type === 'verse number'
+
+	if (doesntCount) {
+		return nextChunkThatMattersNeedsAtLeastOneSpace(tail)
+	}
+
+	return needAtLeastOneSpace(chunk.type)
+}
+
+
+
+function flatMap(ary, mapFn) {
+	const output = []
+
+	ary.forEach(item => {
+		const mapResult = mapFn(item)
+		if (Array.isArray(mapResult)) {
+			mapResult.forEach(item => output.push(item))
+		} else {
+			output.push(mapResult)
+		}
+	})
+
+	return output
+}
+
+function write(file, data) {
+	fs.writeFileSync(`./${file}.json`, JSON.stringify(data, null, '\t'))
+}
+
+function splitVerseIfNecessary(verse) {
+	if (verse.chapterNumber === 12 && verse.verseNumber === 17) {
+		return chunk(verse, [
+			'So the dragon was furious about the woman and off he went to make war with the rest of her offspring,',
+			'those who keep the commands of God and hold the testimony of Jesus.'
+		])
+	} else {
+		return verse
+	}
+}
+
+function chunk(verse, chunks) {
+	return chunks.map(text => Object.assign({}, verse, { text }))
+}
+
+function addVerseSectionNumbers(verses) {
+	let lastVerseNumber = 0
+	let sectionNumber = 0
+
+	return verses.map(chunk => {
+		if (chunk.type !== 'verse') {
+			return chunk
+		}
+
+		if (chunk.verseNumber !== lastVerseNumber) {
+			sectionNumber = 0
+		}
+		sectionNumber++
+		lastVerseNumber = chunk.verseNumber
+		return Object.assign({}, chunk, { sectionNumber })
+	})
+}
+
+function joinVerseChunks(strings) {
+	return strings.reduce((sentence, string) => {
+		const startsWithPunctuation = /^[\.,]/.test(string)
+		const endsWithDash = /[-—]$/.test(sentence)
+
+		const needsLeadingSpace = sentence.length > 0
+			&& !startsWithPunctuation
+			&& !endsWithDash
+
+		const addition = needsLeadingSpace ? (' ' + string) : string
+
+		return sentence + addition
+	})
+}
